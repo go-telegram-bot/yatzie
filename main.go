545d880b003eba1ec018096e36441ea2d9e463e1
mudler <mudler@dark-lab.net>
2015-08-15 15:24:59 +0200

    Adding logging to file and command line options

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/go-telegram-bot/yatzie/shared/registry"
 	"github.com/go-telegram-bot/yatzie/shared/utils"
+	. "github.com/mattn/go-getopt"
 	"github.com/tucnak/telebot"
 
 	_ "github.com/go-telegram-bot/yatzie/plugins/9gag"
@@ -23,9 +26,45 @@ import (
 )
 
 func main() {
-	config, err := util.LoadConfig("telegram-config.json")
+
+	var c int
+	var configurationFile = "telegram-config.json"
+	var logFile = "telebot.log"
+	OptErr = 0
+	for {
+		if c = Getopt("c:l:h"); c == EOF {
+			break
+		}
+		switch c {
+		case 'c':
+
+			configurationFile = OptArg
+		case 'l':
+			logFile = OptArg
+		case 'h':
+			println("usage: " + os.Args[0] + " [-c configfile.json|-l logfile|-h]")
+			os.Exit(1)
+		}
+	}
+
+	config, err := util.LoadConfig(configurationFile)
+
+	//Set logging to file
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	bot, err := telebot.NewBot(config.Token)
-	fmt.Println("Token: " + config.Token)
+	if config.Token != "" {
+		fmt.Println("Token: " + config.Token)
+	}
+	fmt.Println("Configuration file: " + configurationFile)
+	fmt.Println("Log file: " + logFile)
+
 	if err != nil {
 		return
 	}
